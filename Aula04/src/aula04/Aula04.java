10312f02b2b638331bf562ddc3c0f86e659f7201
FRANCISCO CARLOS PIERNA <pierna_f@hotmail.com>
2024-04-05 14:30:41 -0300

    exec 05/04/2024

---
+++ Aula04/src/aula04/Aula04.java
@@ -0,0 +1,13 @@
+package aula04;
+
+public class Aula04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Caneta c1= new Caneta("Nic","Amarelo",0.4f);
+		c1.status();
+		Caneta c2 = new Caneta("kkk","Verde",1.5f);
+        c2.status(); 
+	}
+
+}
