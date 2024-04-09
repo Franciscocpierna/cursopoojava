fce0e0dea0171c2d57ccc64d1f1e6ffeaffd3103
FRANCISCO CARLOS PIERNA <pierna_f@hotmail.com>
2024-04-09 09:58:27 -0300

    exercicio 09/04/2024

--- Aula05/src/aula05/Aula05.java
+++ Aula05/src/aula05/Aula05.java
@@ -4,6 +4,24 @@ public class Aula05 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		ContaBanco p1 = new ContaBanco();
+		p1.setNumconta(1111);
+		p1.setDono("Jubileu");
+		p1.abrirConta("CC");
+		
+		ContaBanco p2 = new ContaBanco();
+		p2.setNumconta(2222);
+		p2.setDono("Cleusa");
+		p2.abrirConta("CP");
+		p1.depositar(100);
+		p2.depositar(500);
+		p2.sacar(100);
+		p2.sacar(1000);
+		p1.sacar(150);
+		p1.fecharConta();
+		p1.estadoAtual();
+		p2.estadoAtual();
+		
 
 	}
 
--- Aula05/src/aula05/ContaBanco.java
+++ Aula05/src/aula05/ContaBanco.java
@@ -7,6 +7,16 @@ private String dono;
 private float saldo;
 private boolean status;
 
+public void estadoAtual() {
+	System.out.println("============================");
+	System.out.println("Conta: "+ this.getNumconta());
+	System.out.println("Tipo: "+this.getTipo());
+	System.out.println("Dono: "+this.getDono());
+	System.out.println("Saldo: "+this.getSaldo());
+	System.out.println("Status "+this.getStatus());
+	
+}
+
 public void abrirConta(String t) {
 	this.setTipo(t);
 	this.setStatus(true);
@@ -29,15 +39,39 @@ public void fecharConta() {
 	
 }
 public void depositar(float v) {
-	if (this.getStatus()==true) {
+	if (this.getStatus()) {
 		this.setSaldo(this.getSaldo()+v);
+		System.out.println("Deposito efetuado com sucesso na conta "+this.getDono());
+	}else {
+		System.out.println("impossível depositar em uma conta fechada");
 	}
 }
-public void sacar() {
+public void sacar(float v) {
+	if(this.getStatus()) {
+		if(this.getSaldo()>=v) {
+			this.setSaldo(this.getSaldo()-v); 
+			System.out.println("Saque realizado na conta de "+this.getDono());
+		}else {
+			System.out.println("saldo insuficiente na conta de "+this.getDono());
+		} 
+	}else {
+		System.out.println("A conta esta fechada do "+this.getDono());
+	}
 	
 }
 public void pagarMensal() {
-	
+	int v=0;
+	if (this.getTipo()=="CC") {
+		v=12;
+	}else if(this.getTipo()=="CP"){
+		v=20;
+	}
+	if(this.getStatus()) {
+		this.setSaldo(this.getSaldo()-v);
+		System.out.println("mensalidade paga com sucesso por "+this.getDono());
+	} else {
+		System.out.println("não foi paga Conta fechada de  "+this.getDono());
+	}
 }
 
 public ContaBanco() {
